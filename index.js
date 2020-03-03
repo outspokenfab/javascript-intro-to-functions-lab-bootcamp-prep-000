e36a16f318bf924d0ef197d796588340dd4e4f05
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-03 20:44:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ var mixedcase = "Hi there!"
 
 function sayHiToGrandma(string){
   console.log(string);
-  if(string.toLowerCase() === lowercase){
+  if(string === lowercase){
   return 'I can\'t hear you!';
-  } else if(string.toUpperCase() === uppercase){
+  } else if(string === uppercase){
     return "YES INDEED!";
   } else {
     return 'I love you, too.';
